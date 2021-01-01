0e6c4bf22375cdd7ff60a3beb76fe737de0cc0f2
Zee <56460839+Mhysth@users.noreply.github.com>
2021-01-01 12:43:35 +0700

    adapter error

--- app/src/main/java/com/example/uas/model/Report.java
+++ app/src/main/java/com/example/uas/model/Report.java
@@ -1,4 +1,30 @@
 package com.example.uas.model;
 
-public class Report {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Report implements Parcelable {
+    protected Report(Parcel in) {
+    }
+
+    public static final Creator<Report> CREATOR = new Creator<Report>() {
+        @Override
+        public Report createFromParcel(Parcel in) {
+            return new Report(in);
+        }
+
+        @Override
+        public Report[] newArray(int size) {
+            return new Report[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
 }
