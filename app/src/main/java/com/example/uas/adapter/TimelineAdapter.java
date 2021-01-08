84b7de3ed5cd38a13572ea1498093f163e17881e
Zee <56460839+Mhysth@users.noreply.github.com>
2021-01-08 23:06:18 +0700

    reminder

--- app/src/main/java/com/example/uas/adapter/TimelineAdapter.java
+++ app/src/main/java/com/example/uas/adapter/TimelineAdapter.java
@@ -1,13 +1,15 @@
 package com.example.uas.adapter;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.uas.R;
 import com.example.uas.model.local.Timeline;
 
--- app/src/main/java/com/example/uas/fragment/NotificationFragment.java
+++ app/src/main/java/com/example/uas/fragment/NotificationFragment.java
@@ -1,24 +1,22 @@
 package com.example.uas.fragment;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import java.util.Calendar;
 
-import com.example.uas.R;
-
-public class NotificationFragment extends Fragment {
-
-    public NotificationFragment() {
-        // Required empty public constructor
-    }
+public class NotificationFragment extends DialogFragment {
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
     }
 }
