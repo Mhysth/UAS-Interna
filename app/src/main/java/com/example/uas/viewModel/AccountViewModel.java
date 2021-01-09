d2671f514f1e09d400a1adda75a4a41cf321b801
Bryant97-dev <62684129+Bryant97-dev@users.noreply.github.com>
2021-01-10 02:15:16 +0700

    account berhasil(photo belum selesai)

--- app/src/main/java/com/example/uas/viewModel/AccountViewModel.java
+++ app/src/main/java/com/example/uas/viewModel/AccountViewModel.java
@@ -5,7 +5,13 @@ import android.util.Log;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.uas.model.local.Timeline;
+import com.example.uas.model.local.User;
 import com.example.uas.repository.AuthRepository;
+import com.example.uas.repository.TimelineRepository;
+import com.example.uas.repository.UserRepository;
+
+import java.util.List;
 
 public class AccountViewModel extends ViewModel {
 
@@ -19,6 +25,33 @@ public class AccountViewModel extends ViewModel {
     public LiveData<String> logout() {
         return repository.logout();
     }*/
+
+    private UserRepository repository;
+    private static final String TAG = "AccountViewModel";
+
+    public AccountViewModel() {
+
+    }
+
+    public void init(String token) {
+        Log.d(TAG, "init: " + token);
+        repository = UserRepository.getInstance(token);
+    }
+
+    public LiveData<List<User>> getUser() {
+        return repository.getUser();
+    }
+
+   /* public LiveData<String> logout() {
+        return repository.logout();
+    }*/
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.d(TAG, "onCleared: ");
+        repository.resetInstance();
+    }
 
 
 }
