24b058ae3cf7b7f90392530d47d03f0f5c63f011
Zee <56460839+Mhysth@users.noreply.github.com>
2021-01-18 00:32:51 +0700

    fresh merge from feat. and dev.

---
+++ app/src/main/java/com/example/uas/model/response/EventResponse.java
@@ -0,0 +1,16 @@
+package com.example.uas.model.response;
+
+
+import com.example.uas.model.local.Timeline;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class EventResponse {
+    /*@SerializedName("data")
+    private List<Timeline> results;
+
+    public List<Timeline> getResults() {
+        return results;
+    }*/
+}
