ccd79db58fe92cd2c4f1e9c3db5c02dfcf354b44
Zee <56460839+Mhysth@users.noreply.github.com>
2021-01-21 03:59:53 +0700

    s

--- README.md
+++
@@ -1,5 +0,0 @@
-# Interna
-
-Aplikasi Android untuk memudahkan pencatatan mahasiswa magang (mahasiswa semester akhir).
-
-UAS Mobile Application Development - Informatika Universitas Ciputra
--- app/src/main/java/com/example/uas/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.uas.utils;
-public class Constants {
-//    public final static String BASE_URL = "http://7a780cc36a5e.ngrok.io/api/";
-    public final static String BASE_URL = "http://10.214.32.193/UAS-Interna/public/api/";
-//    public final static String BASE_URL = "http://192.168.1.135:8080/Interna-develop/public/api/";
-}
