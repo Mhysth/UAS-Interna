c1b5a309e13a92527fc7baaf894292170e7b732f
Zee <56460839+Mhysth@users.noreply.github.com>
2021-01-20 13:38:00 +0700

    animation

--- app/src/main/java/com/example/uas/utils/Constants.java
+++ app/src/main/java/com/example/uas/utils/Constants.java
@@ -2,6 +2,6 @@ package com.example.uas.utils;
 
 public class Constants {
     public final static String BASE_URL = "http:/10.214.51.182/UAS-Interna/public/api/";
-//   public final static String BASE_URL = "http://192.168.1.135:8080/Interna-develop/public/api/";
+//  public final static String BASE_URL = "http://192.168.1.135:8080/Interna-develop/public/api/";
 //    public final static String BASE_URL = "http://4d0491bac2bd.ngrok.io/api/";
 }
